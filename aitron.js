2b5b2884debd7af9f8cab7126351b06802b02e1e
Tony Zou <tonythemoose@gmail.com>
2011-07-19 20:05:51 -0400

    I'm cool

--- aitron.js
+++ aitron.js
@@ -4,7 +4,14 @@ var bs = {
 	"height": 35
 };
 
+var canvas, ctx;
 
+var loadCanvas = function() {
+	canvas = document.getElementById("canvas");
+	canvas.setAttribute("width", bs.width.toString());
+	canvas.setAttribute("height", bs.height.toString());
+	ctx = canvas.getContext("2d");
+}
 
 
 
